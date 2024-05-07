ab296af1e299746799d785eee01c2c3acefe7cd3
BasilLiao <BasilLiao@Basil>
2024-05-07 17:30:09 +0800

    Beta v0.01.34+++
    SQL

---
+++ dtriCloud/service-basic-12020/src/main/resources/SQLQuery_Cloud_製令單排程.sql
@@ -0,0 +1,34 @@
+SELECT --
+  ROW_NUMBER() OVER (ORDER BY CTA.TA001) AS MOCTA_ID,--
+  REPLACE(CTA.TA001+'-'+CTA.TA002, ' ', '') AS TA001_TA002,--製令單
+  CTA.TA006,--產品品號
+  CTA.TA034,--產品品名
+  CTA.TA035,--產品規格
+  CTA.TA015,--預計生產數
+  CTA.TA017,--目前生產數
+  CTA.TA009,--預計開工日
+  CTA.TA010,--預計完工日
+  CTA.TA011,--狀態碼1.未生產,2.已發料,3.生產中,Y.已完工,y.指定完工
+  CTA.TA029,--製令備註(客戶/國家/訂單)
+  CTA.TA054,--製令-自訂義備註(自動帶出)
+  CTA.CREATOR,--創建工單使用者
+  CTA.CREATE_DATE,--單據建立時間
+  CTA.MODI_DATE,--單據修改時間
+  CTA.TA032,--加工廠(代號)
+  ISNULL(PUR.MA002,'') AS MA002,--加工廠(中文)
+  PTD.TD004,--客戶品號
+  PTC.TC012,--客戶-訂單單號
+  PTC.TC001+'-'+PTC.TC002 AS TC001_TC002 --公司-訂單單號
+FROM --
+[DTR_TW].[dbo].MOCTA AS CTA --
+     LEFT JOIN [DTR_TW].[dbo].COPTD AS PTD ON (REPLACE(CTA.TA026+'-'+CTA.TA027+'-'+CTA.TA028, ' ', '') = REPLACE(PTD.TD001+'-'+PTD.TD002+'-'+PTD.TD003, ' ', ''))
+     LEFT JOIN [DTR_TW].[dbo].COPTC AS PTC ON (PTC.TC001+PTC.TC002 = PTD.TD001+PTD.TD002)
+     LEFT JOIN [DTR_TW].[dbo].PURMA AS PUR ON (PUR.MA001 = CTA.TA032)
+WHERE --
+	(CTA.TA011 = '1' OR CTA.TA011 = '2' OR CTA.TA011 = '3')
+	AND (CTA.TA013 = 'Y')
+	AND (CTA.TA006 LIKE '81-105%')
+	AND ((CTA.TA001 = 'A511') OR (CTA.TA001= 'A521')-- 廠內 一般/重工製令單
+	OR (CTA.TA001 = 'A512') OR (CTA.TA001= 'A522'))-- 廠外 一般/重工製令單
+ORDER BY --
+	CTA.TA001+CTA.TA002 ASC
