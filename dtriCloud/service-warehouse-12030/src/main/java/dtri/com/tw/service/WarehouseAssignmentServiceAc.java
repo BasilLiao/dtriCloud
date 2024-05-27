3c937880285b58726c572b3867749e1726749a77
BasilLiao <BasilLiao@Basil>
2024-05-27 17:43:47 +0800

    Beta v0.01.36+++++++
    修復 小問題

--- dtriCloud/service-warehouse-12030/src/main/java/dtri/com/tw/service/WarehouseAssignmentServiceAc.java
+++ dtriCloud/service-warehouse-12030/src/main/java/dtri/com/tw/service/WarehouseAssignmentServiceAc.java
@@ -122,7 +122,7 @@ public class WarehouseAssignmentServiceAc {
 			 * 0, null, inPageable);
 			 */
 			ArrayList<BasicShippingList> shippingLists = shippingListDao.findAllBySearchStatus("A541", null, null, null,
-					"true", null, "false", 0, null, shPageable);
+					"true", null, null, 0, null, shPageable);
 			// 進料
 			incomingLists.forEach(in -> {
 				String headerKey = in.getBilclass() + "-" + in.getBilsn();
